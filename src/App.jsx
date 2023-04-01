8377401102513e37ae562ec9c9dd83e4ffbd6f2d
Tamani Phiri <tamanigabriel0@gmail.com>
2023-04-01 14:17:57 +0200

    navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import  NavBar from './Components/Navbar'
+
+
+function App() {
+
+
+  return (
+    <div className=' items-center justify-center'>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
