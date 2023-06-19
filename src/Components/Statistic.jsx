f395c3a15e24d6829cc53f4ca10bd070b0aca945
Tamani Phiri <tamanigabriel0@gmail.com>
2023-06-19 11:13:36 +0200

    improvements

--- src/Components/Statistic.jsx
+++ src/Components/Statistic.jsx
@@ -6,7 +6,7 @@ const Statistic = () => {
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-[#1f2937]">
             Master Cleanse Reliac Heirloom
           </h1>
-          <p className="lg:w-2/3 mx-auto text-gray-500 leading-relaxed text-base">
+          <p className="lg:w-2/3 mx-auto text-gray-500 leading-relaxed text-sm md:text-base">
             Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
             gentrify, subway tile poke farm-to-table. Franzen you probably
             haven't heard of them man bun deep jianbing selfies heirloom prism
